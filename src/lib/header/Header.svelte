4f7c4f8cca57bee2dd2713b003e232927a69d362
Stephen Jennings <stephen.g.jennings@gmail.com>
2022-03-09 16:10:49 -0800

    remove about and todos pages

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -16,14 +16,8 @@
 		</svg>
 		<ul>
 			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
-			</li>
 			<li class:active={$page.url.pathname === '/json'}>
 				<a sveltekit:prefetch href="/json">JSON</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Todos</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
