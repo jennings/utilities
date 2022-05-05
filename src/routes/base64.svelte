5f754589ff5820a6151a220ffba0f0af8c34fd24
Stephen Jennings <stephen.g.jennings@gmail.com>
2022-05-05 10:57:54 -0700

    base64: add button to format deocded text as JSON

--- src/routes/base64.svelte
+++ src/routes/base64.svelte
@@ -9,6 +9,7 @@
 		| { type: "custom"; char63: string; char64: string };
 
 	let alphabet: Alphabet = { type: "base64" };
+	let tryFormatDecodedAsJson = true;
 
 	$: [char63, char64] = getChars(alphabet);
 
@@ -16,7 +17,10 @@
 
 	let input: UserInput = { type: "text", value: "" };
 
-	$: text = input.type === "text" ? input.value : decode(input.value, char63, char64);
+	$: text =
+		input.type === "text"
+			? input.value
+			: decode(input.value, char63, char64, tryFormatDecodedAsJson);
 	$: encoded = input.type === "encoded" ? input.value : encode(input.value, char63, char64);
 
 	function getChars(alphabet: Alphabet): [string, string] {
@@ -43,10 +47,18 @@
 		}
 	}
 
-	function decode(t: string, char63: string, char64: string) {
+	function decode(t: string, char63: string, char64: string, tryFormatDecodedAsJson: boolean) {
 		try {
 			const base64 = t.replace(char63, STD_CHAR_63).replace(char64, STD_CHAR_64);
-			return atob(base64);
+			const decoded = atob(base64);
+			if (!tryFormatDecodedAsJson) {
+				return decoded;
+			}
+			try {
+				return JSON.stringify(JSON.parse(decoded), null, 2);
+			} catch (err) {
+				return decoded;
+			}
 		} catch (err) {
 			return `${err}`;
 		}
@@ -66,98 +78,109 @@
 </svelte:head>
 
 <div>
-	<h1>Base 64</h1>
-
-	<blockquote>
-		<p class="secret">Psst... You could also do this with:</p>
-		<ul>
-			<li>
-				Windows:
-				<ul>
-					<li>
-						Encode: <code on:click={highlightThis}>
-							[convert]::ToBase64String([system.text.encoding]::UTF8.GetBytes((get-clipboard)))
-						</code>
-					</li>
-					<li>
-						Decode: <code on:click={highlightThis}>
-							[system.text.encoding]::UTF8.ToString([convert]::FromBase64String((Get-Clipboard)))
-						</code>
-					</li>
-				</ul>
-			</li>
-			<li>
-				macOS:
-				<ul>
-					<li>
-						Encode: <code on:click={highlightThis}>pbpaste | base64</code>
-					</li>
-					<li>
-						Decode: <code on:click={highlightThis}>pbpaste | base64 -D</code>
-					</li>
-				</ul>
-			</li>
-		</ul>
-	</blockquote>
-
 	<div>
-		<h2>Alphabet</h2>
-		<label>
-			<input
-				type="radio"
-				name="alphabet"
-				checked={alphabet.type === "base64"}
-				on:change={(e) => {
-					if (e.currentTarget.checked) {
-						alphabet = { type: "base64" };
-					}
-				}}
-			/>
-			Base 64
-		</label>
-		<br />
-		<label>
-			<input
-				type="radio"
-				name="alphabet"
-				checked={alphabet.type === "base64safe"}
-				on:change={(e) => {
-					if (e.currentTarget.checked) {
-						alphabet = { type: "base64safe" };
-					}
-				}}
-			/>
-			File and URL safe
-		</label>
-		<br />
-		<input
-			type="radio"
-			name="alphabet"
-			checked={alphabet.type === "custom"}
-			on:change={(e) => {
-				if (e.currentTarget.checked) {
-					alphabet = { type: "custom", char63, char64 };
-				}
-			}}
-		/>
-		<input
-			style="width: 2em"
-			title="Char 63"
-			maxlength="1"
-			value={char63}
-			on:input={(e) => {
-				alphabet = { type: "custom", char64, char63: e.currentTarget.value };
-			}}
-		/>
-		<input
-			style="width: 2em"
-			title="Char 64"
-			maxlength="1"
-			value={char64}
-			on:input={(e) => {
-				alphabet = { type: "custom", char63, char64: e.currentTarget.value };
-			}}
-		/>
+		<h1>Base 64</h1>
+
+		<blockquote>
+			<p class="secret">Psst... You could also do this with:</p>
+			<ul>
+				<li>
+					Windows:
+					<ul>
+						<li>
+							Encode: <code on:click={highlightThis}>
+								[convert]::ToBase64String([system.text.encoding]::UTF8.GetBytes((get-clipboard)))
+							</code>
+						</li>
+						<li>
+							Decode: <code on:click={highlightThis}>
+								[system.text.encoding]::UTF8.ToString([convert]::FromBase64String((Get-Clipboard)))
+							</code>
+						</li>
+					</ul>
+				</li>
+				<li>
+					macOS:
+					<ul>
+						<li>
+							Encode: <code on:click={highlightThis}>pbpaste | base64</code>
+						</li>
+						<li>
+							Decode: <code on:click={highlightThis}>pbpaste | base64 -D</code>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</blockquote>
+
+		<div class="options">
+			<div>
+				<h2>Alphabet</h2>
+				<label>
+					<input
+						type="radio"
+						name="alphabet"
+						checked={alphabet.type === "base64"}
+						on:change={(e) => {
+							if (e.currentTarget.checked) {
+								alphabet = { type: "base64" };
+							}
+						}}
+					/>
+					Base 64
+				</label>
+				<br />
+				<label>
+					<input
+						type="radio"
+						name="alphabet"
+						checked={alphabet.type === "base64safe"}
+						on:change={(e) => {
+							if (e.currentTarget.checked) {
+								alphabet = { type: "base64safe" };
+							}
+						}}
+					/>
+					File and URL safe
+				</label>
+				<br />
+				<input
+					type="radio"
+					name="alphabet"
+					checked={alphabet.type === "custom"}
+					on:change={(e) => {
+						if (e.currentTarget.checked) {
+							alphabet = { type: "custom", char63, char64 };
+						}
+					}}
+				/>
+				<input
+					style="width: 2em"
+					title="Char 63"
+					maxlength="1"
+					value={char63}
+					on:input={(e) => {
+						alphabet = { type: "custom", char64, char63: e.currentTarget.value };
+					}}
+				/>
+				<input
+					style="width: 2em"
+					title="Char 64"
+					maxlength="1"
+					value={char64}
+					on:input={(e) => {
+						alphabet = { type: "custom", char63, char64: e.currentTarget.value };
+					}}
+				/>
+				<br />
+			</div>
+			<div>
+				<h2>Options</h2>
+				<label>
+					<input type="checkbox" bind:checked={tryFormatDecodedAsJson} /> Format decoded text as JSON
+				</label>
+			</div>
+		</div>
 
 		<h2>Text</h2>
 		<textarea
@@ -186,5 +209,13 @@
 <style>
 	.secret {
 		font-style: italic;
+	}
+	.options {
+		display: flex;
+		flex-direction: row;
+		gap: 2em;
+	}
+	.options > * {
+		flex-grow: 0;
 	}
 </style>
